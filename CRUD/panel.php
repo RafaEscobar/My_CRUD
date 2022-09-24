fce018f4e162864c3c7c9609701ccfbcada100c4
Rafael Escobar Gutierrez <Rafael.EG.code@gmail.com>
2022-09-23 21:08:43 -0500

    Más comentarios, edicion de comentarios, y nuevo orden de comentarios -LISTO-

--- CRUD/panel.php
+++ CRUD/panel.php
@@ -12,7 +12,6 @@
     */
     $resul_consul = mysqli_query($conex, $consul_select);
     
-
 ?>
 
 
@@ -24,13 +23,16 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../CSS/styles.css">
-    <title>Document</title>
+    <title>Panel de control</title>
 </head>
 
 <body>
     <h1>Panel de control</h1>
+    <!-- Creamos una caja para nuestra tabla CRUD -->
     <div class="tabla">
+        <!-- Abrimos la tabla -->
         <table>
+            <!-- Establecemos una cabecera para la tabla -->
             <thead>
                 <tr>
                     <th>Nombre</th>
@@ -40,32 +42,45 @@
                 </tr>
             </thead>
             <tbody>
-                <!-- #4: Generamos un WHILE el cual se ejecutara hasta que la funcion 
-                        //mysqli_fetch_assoc// ya no reciba ninguna fila.-->
-                        <!-- ESTE WHILE AL EJECUTARSE MUESTRA EN CADA VUELTA LOS
-                             VALORES SOLICITADOS, QUE ESTE EN LA FILA DE TURNO -->
-                             <!-- Los valores solicitados, rescatados, se almacenan
-                                  en una variable llamada //fila//, como un array
-                                  asociativo -->
+                <!-- 
+                    #4: Generamos un WHILE el cual se ejecutara hasta que la funcion 
+                    --mysqli_fetch_assoc-- ya no reciba ninguna fila.
+                -->
+                <!-- 
+                    ESTE WHILE AL EJECUTARSE MUESTRA EN CADA VUELTA LOS
+                    VALORES SOLICITADOS, QUE ESTE EN LA FILA DE TURNO 
+                -->
+                <!-- 
+                    Los valores rescatados con la funcion antes mencionada, 
+                    se almacenan en una variable llamada --fila--, como un 
+                    array asociativo 
+                -->
+                    <!-- PARA GENERAR EL WHILE TENEMOS QUE ABRIR ETIQUETAS PHP -->
                 <?php
                     while($fila=mysqli_fetch_assoc($resul_consul)){
                 ?>
                 <tr>
-                    <!-- <?php //print_r($fila); ?> -->
-                    <!-- #5: Con ayuda del array asociativo, el dato el cual a su ves llama
-                         a su correspondiente valor, mostramos por pantalla ( en la tabla )
-                         los valores -->
+                    <!-- <?php //print_r($fila); ?>  --> <!-- para ver el array asociativo-->
+                    <!-- 
+                        #5: Con ayuda del array asociativo y el dato (el cual a su ves llama a su correspondiente valor), mostramos en la tabla los valores
+                    -->
                 <!-- 
                    >> EJEMPLO DE COMO SE VE EL ARRAY ASOCIATIVO
                    Array ( [User_Id] => 1 [User_nom] => Rafa [User_email] => rafa@gmail.com ) 
                    Array ( [User_Id] => 2 [User_nom] => Jaime [User_email] => jaimito@gmail.com )
                    Array ( [User_Id] => 3 [User_nom] => Balboa [User_email] => balbo@gmail.com )
                  -->
+                            <!-- 
+                                LOS TD SON CELDAS, Y CADA UNA REPRESENTA UN CAMPO, EN ESTE EJEMPLO TENEMOS DOS CAMPOS, -Nombre- y -Email-
+                            -->
                     <td><?php echo $fila['User_nom']; ?></td>
                     <td><?php echo $fila['User_email'] ?></td>
+                    <!-- 
+                        #6: Enviamos manualmente con el principio del metodo GET, el valor del ID rescatado de la BD que corresponde a la fila de la cual el btn EDITAR O BORRAR haya sido pulsado.-->
+                            <!-- 
+                                EN CADA VUELTA EL VALOR DEL ID QUE SE RESCATA DE LA BASE DE DATOS SE ALMACENA* EN LA URL QUE SE CONTRUYE AQUI EN CADA BTN  
+                            -->
                     <td>
-                        <!-- #6: Enviamos manualmente con el principio del metodo GET, el valor del ID rescatado de la BD que corresponde a la fila de la cual el btn EDITAR  O BORRAR haya sido pulsado.-->
-                             <!-- EN CADA VUELTA EL VALOR DEL ID QUE SE RESCATA DE LA BASE DE DATOS SE ALMACENA* EN LA URL QUE SE CONTRUYE AQUI EN CADA BTN  -->
                         <?php echo "<a href='../CRUD/edit.php?User_Id=".$fila['User_Id']."'>EDITAR</a>"; ?>
                     </td>
                     <td>
@@ -73,8 +88,8 @@
                     </td>
                 </tr>
 
+                <!-- #7: CERRAMOS LA LLAVE DEL WHILE, Y PARA ESO ABRIMOS ETIQUETAS PHP-->
                 <?php
-                // #7: CERRAMOS LA LLAVE DEL WHILE
                     }
                 ?>
             </tbody>
